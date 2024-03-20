b8923a132e12c46976a970e7651690be9f82a714
JoshSan1408 <104320386+Joshy14Y@users.noreply.github.com>
2024-03-20 13:12:07 -0600

    UI - Menus

---
+++ Assets/Oculus Hands/Assets_Leo/controlCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject Camera1;
+    public GameObject Camera2;
+    public GameObject Camera3;
+
+
+    
+
+    public void CameraOne()
+    {
+        Camera1.SetActive(true);
+        Camera2.SetActive(false);
+        Camera3.SetActive(false);
+    }
+
+    public void CameraTwo()
+    {
+        Camera3.SetActive(false);
+        Camera2.SetActive(true);
+        Camera1.SetActive(false);  
+    }
+
+    void CameraThree()
+    {
+        Camera3.SetActive(true);
+        Camera2.SetActive(false);  
+        Camera1.SetActive(false);
+    }
+}
